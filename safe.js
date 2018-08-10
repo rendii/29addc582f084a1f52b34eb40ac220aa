ce0a740314bd6c530f316f17860a3cfb782842a8
rendii <xibiren21@gmail.com>
2018-08-10 19:29:51 +0800

    Update safe.js

--- safe.js
+++ safe.js
@@ -18,14 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
-var xxx=null;
-if(x=="1"){xxx="2018/08/sd.html"}
-if(x=="2"){xxx="2018/08/dfs.html"}
-if(x=="3"){xxx="2018/08/oc.html"}
-if(x=="4"){xxx="2018/08/asw.html"}
-if(x=="5"){xxx="p/go.html"}a_to_ve[i].href="https://flynn-me.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://flynn-me.blogspot.com?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
