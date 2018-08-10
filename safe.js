ed2debbf2b7a85f0ee1f7a9e562f94ff8ab22a87
rendii <xibiren21@gmail.com>
2018-08-10 18:57:33 +0800

    Update safe.js

--- safe.js
+++ safe.js
@@ -21,12 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="come-on-learn-coding-to-not-rip-off.html"}
-if(x=="2"){xxx="difficult-to-freedom-in-era-of-electric.html"}
-if(x=="3"){xxx="6-things-you-must-know-about-travel.html"}
-if(x=="4"){xxx="seven-ways-to-maintain-eye-health_11.html"}
-if(x=="5"){xxx="/p/go.html"}
-if(x=="5"){xxx="go-to-url.html"}a_to_ve[i].href="https://flynn-me.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="2018/08/sd.html"}
+if(x=="2"){xxx="2018/08/dfs.html"}
+if(x=="3"){xxx="2018/08/oc.html"}
+if(x=="4"){xxx="2018/08/asw.html"}
+if(x=="5"){xxx="p/go.html"}a_to_ve[i].href="https://flynn-me.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
